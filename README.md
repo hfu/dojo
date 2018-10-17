eccf90b9852a6fcbe8db219877cca4c87cb6d08e
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-10-16 21:45:29 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dojo
+
+## source data
+[縮尺5万分の１農耕地包括土壌図](https://soil-inventory.dc.affrc.go.jp/download5.html)
+
+## data license
+[CC BY 4.0](http://creativecommons.org/licenses/by/4.0/) by 農業・食品産業技術総合研究機構農業環境変動研究センター
+
+## how the mbtiles is built
+```console
+$ git clone git@github.com:hfu/dojo
+$ cd dojo
+$ mkdir src
+$ (download the zip file and extract the shepfile)
+$ npm install
+$ node index.js > dojo.ndjson
+$ tippecanoe -f -o dojo.mbtiles --maximum-zoom=12 --name=縮尺5万分の１農耕地包括土壌図 --attribution=農業・食品産業技術総合研究機構農業環境変動研究センター dojo.ndjson
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const config = require('config')
+const shapefile = require('shapefile')
+const geojsonArea = require('@mapbox/geojson-area')
+const F = 21
+
+const dump = (f) => {
+  f.properties.code = f.properties['統群コード']
+  f.tippecanoe = {
+    layer: 'dojo',
+    minzoom: 12,
+    maxzoom: 12
+  }
+  console.log(JSON.stringify(f))
+
+  f.tippecanoe.minzoom = Math.floor(
+    F - Math.log2(geojsonArea.geometry(f.geometry)) / 2
+  )
+  if (f.tippecanoe.minzoom < 7) f.tippecanoe.minzoom = 7
+  f.tippecanoe.maxzoom = 11
+  if (f.tippecanoe.minzoom > 11) f.tippecanoe.minzoom = 11
+  for (const key of Object.keys(f.properties)) {
+    if (key !== 'code') {
+      delete f.properties[key]
+    }
+  }
+  console.log(JSON.stringify(f))
+}
+
+for (const source of config.get('sources')) {
+  shapefile.open(
+    source, 
+    source.replace(/shp$/, 'dbf'), 
+    { encoding : config.get('encoding') }
+  ).then(s => s.read()
+      .then(function log(r) {
+        if (r.done) return
+        dump(r.value)
+        return s.read().then(log)
+      }))
+    .catch(err => {
+      console.error(err.stack)
+    })
+}
+
